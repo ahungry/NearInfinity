5ca530ab5f47a39cc0559303e3cd1d1f709d1f1e
Matthew Carter <m@ahungry.com>
2021-12-19 00:53:30 -0500

    Need to keep trying, getting closer perhaps

--- decompress.js
+++ decompress.js
@@ -42,18 +42,13 @@ function get_entry (buf, offset) {
   }
 }
 
+// Build all the entries - equivalent of the .sav file
 while (offset < buf.length) {
   const entry = get_entry(buf, offset)
 
   offset = entry.offset
   entries[entry.fileName] = entry
 }
-
-// console.log('end offset: ', offset)
-// console.log(entries['THBAG05.sto'])
-
-// console.log(header)
-// console.log(entries)
 
 function sort_bag(key) {
   const x = entries[key].cdata
@@ -98,14 +93,40 @@ function sort_bag(key) {
   return { unsortedEntry: z, sortedEntry }
 }
 
-const test = sort_bag('THBAG05.sto')
+const sortedBag = sort_bag('THBAG05.sto')
 // sort_bag('THBAG03.sto')
 // sort_bag('THBAG01.sto')
 
-let fh = fs.openSync('test_unsorted.sto', 'w')
-fs.writeSync(fh, test.unsortedEntry, 0, test.unsortedEntry.length, 0)
-fs.closeSync(fh)
+// test more readable files
+// let fh = fs.openSync('test_unsorted.sto', 'w')
+// fs.writeSync(fh, test.unsortedEntry, 0, test.unsortedEntry.length, 0)
+// fs.closeSync(fh)
 
-fh = fs.openSync('test_sorted.sto', 'w')
-fs.writeSync(fh, test.sortedEntry, 0, test.sortedEntry.length, 0)
-fs.closeSync(fh)
+// fh = fs.openSync('test_sorted.sto', 'w')
+// fs.writeSync(fh, test.sortedEntry, 0, test.sortedEntry.length, 0)
+// fs.closeSync(fh)
+
+// Ok, so buf is the entire .sav file
+// entries contains the broken up contents in memory
+// we can fetch/sort a bag easily enough
+const bagInfo = entries['THBAG05.sto']
+const newCdata = zlib.deflateSync(sortedBag.sortedEntry)
+console.log(bagInfo)
+console.log(newCdata.length)
+
+// Try to add newCdata in place of old
+function updateCdata (entry, newCdata) {
+  console.log(entry)
+  for (let i = entry.offset; i < entry.offset + entry.compressedLength; i++) {
+    buf[i] = newCdata[i]
+  }
+}
+
+updateCdata(bagInfo, newCdata)
+
+// Well, it produces a .sav the BG game can open, but NI will crash on.
+// Also, it unfortunately doesn't seem to have altered the order of bag contents at all...
+
+// const fh = fs.openSync('hax.sav', 'w')
+// fs.writeSync(fh, buf, 0, buf.length, 0)
+// fs.closeSync(fh)
